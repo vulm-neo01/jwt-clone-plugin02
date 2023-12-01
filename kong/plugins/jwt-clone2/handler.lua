39cf5e99110512a9b7647b205f95b47c72406c41
neoneo2001 <minhvuneo2001@gmail.com>
2023-12-01 05:23:58 -0500

    Complete basic logic

--- kong/plugins/jwt-clone2/handler.lua
+++ kong/plugins/jwt-clone2/handler.lua
@@ -1,6 +1,5 @@
 local jwt_decoder = require "kong.plugins.jwt.jwt_parser"
 local ngx_re_gmatch = ngx.re.gmatch
-local cjson = require "cjson"
 
 local plugin = {
   PRIORITY = 2000, -- set the plugin priority, which determines plugin execution order
@@ -41,11 +40,10 @@ end
 
 -- runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
-  local set_header = kong.service.request.set_header
+  -- local set_header = kong.service.request.set_header
+  local token, err1 = retrieve_token(kong.request, plugin_conf)
 
-  local token, err = retrieve_token(kong.request, plugin_conf)
-
-  if err then
+  if err1 then
     return kong.response.exit(500, "jwt-auth -- Error when retrieving token")
   end
   kong.log.debug(token)
@@ -53,34 +51,75 @@ function plugin:access(plugin_conf)
   local network_type = plugin_conf.network_type or ""
   local zone_id = plugin_conf.zone_id or ""
   local lang = plugin_conf.lang or ""
-  local ips = plugin.verified_IPs or {}
-  local blocked_devices = plugin.blocked_devices or {}
+  local ips = plugin_conf.verified_IPs or {}
+  local blocked_devices = plugin_conf.blocked_devices or {}
 
   local request_ip = kong.client.get_forwarded_ip()
 
   local isIPMatch = false
 
+  kong.log.debug("Request IP: ", request_ip)
   for _, ip in ipairs(ips) do
+      kong.log.debug("Checking IP: ", ip)
       if ip == request_ip then
           isIPMatch = true
           break
       end
   end
 
-  if isIPMatch then
-      kong.response.exit(403, "jwt-auth - Forbidden: IP not allowed")
+  if not isIPMatch then
+      kong.response.exit(403, "jwt-auth - Forbidden: IP not allowed: "..request_ip)
   end
 
   if token then
+    local jwt, err2 = jwt_decoder:new(token)
+    if err2 then
+      kong.log.err("Fail to decode token!")
+      return kong.response.exit(500, "jwt-auth - Token was found, but failed to decoded")
+    end
 
+    local jwt_claims = jwt.claims
+    local headers = kong.request.get_headers()
+    kong.log.debug(jwt_claims)
+
+    local isBlockedDevice = false
+    local dvi = jwt_claims.dvi
+    local userId = jwt_claims.userId or ""
+    local profileId = jwt_claims.profileId or ""
+    local gname = jwt_claims.gname or ""
+    local contentFilter = jwt_claims.contentFilter or ""
+
+    if not dvi then
+      kong.log.err("Cant find device code!")
+      return kong.response.exit(500, "jwt-auth - Cant find device code from token")
+    end
+
+    kong.log.debug("DVI:"..dvi)
+    for _, block_dvi in ipairs(blocked_devices) do
+      kong.log.debug("Dvi blocked: "..block_dvi)
+      if dvi == block_dvi then
+        isBlockedDevice = true
+        break
+      end
+    end
+
+    if isBlockedDevice then
+      kong.response.exit(403, "Device is blocked")
+    end
+
+    kong.log.debug(headers)
+
+    kong.response.set_header("Zone-id", zone_id)
+    kong.response.set_header("User-Id", userId)
+    kong.response.set_header("Profile-Id", profileId)
+    kong.response.set_header("Content-Filter",contentFilter)
+    kong.response.set_header("Gname",gname)
+    kong.response.set_header("dvi",dvi)
+    kong.response.set_header("Network-type", network_type)
+    kong.response.set_header("X-Forwarded-Request-IP", kong.client.get_forwarded_ip())
+    kong.response.set_header("X-Request-IP", kong.client.get_ip())
+    kong.response.set_header("Language", lang)
   end
-
-  kong.response.set_header("X-Forwarded-Request-IP", kong.client.get_forwarded_ip())
-  kong.response.set_header("X-Request-IP", kong.client.get_ip())
-  kong.response.set_header("Config-Zone-id", plugin_conf.zone_id)
-  kong.response.set_header("Config-Network-type", plugin_conf.network_type)
-  kong.response.set_header("Config-Language", plugin_conf.lang)
-  kong.response.set_header("Config-list-Ip", plugin_conf.verified_IPs[1])
 end
 
 return plugin
